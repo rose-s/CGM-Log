307266158e9bb8194a6ca38bfddebadc0c42389a
Rose Servis <roseservis@gmail.com>
2016-04-17 19:10:29 -0700

    more file adds

---
+++ client/log.js
@@ -0,0 +1,54 @@
+SMS = new Mongo.Collection('sms');
+
+
+
+Template.smsLog.helpers({
+  sms: function () {
+    return SMS.find({}, {sort: {dateCreated: -1}});
+  },
+  isIncoming: function () {
+    return this.type === "incoming"
+  }
+});
+
+Template.registerHelper('formatDate', function(date_sent) {
+  return moment(date_sent).format('M/DD/YY');
+});
+
+Template.registerHelper('formatDay', function(date_sent) {
+  return moment(date_sent).format('ddd');
+});
+
+Template.registerHelper('formatTime', function(date_sent) {
+  return moment(date_sent).format('h:mma');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
